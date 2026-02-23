6320c831dcf8e781fb0fafd9f8f192573730b543
vahid2104 <vahid.aliyev.03@mail.ru>
2026-02-23 10:51:53 +0100

    feat: add category preview section to home page and related style to home.css

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom"
 import { Button } from "../components/Button"
-import { ArrowRight, Shield, Truck, Zap } from "lucide-react"
+import { ArrowRight, Headphones, Laptop, Shield, Smartphone, Truck, Zap } from "lucide-react"
 import '../styles/home.css'
 function Home() {
   return (
     <div style={{minHeight: "100vh"}}>
       {/* Hero Section */}
-      <section className="w-full hero-section">
+      <section className="hero-section">
         <div className="container flex items-center justify-between home-hero-section">
           <div className="home-hero-left">
             <h1 className="text-4xl text-bold margin-b">Discover the Latest in Texhnology</h1>
@@ -31,7 +31,7 @@ function Home() {
         </div>
       </section>
       {/* Features */}
-      <section className="w-full bg-color-gray" style={{padding: "10vh 0"}} >
+      <section className="bg-color-gray">
          <div className="container flex flex-wrap justify-between">
           <div className="feature-box">
             <div className="feature-icon-box">
@@ -61,6 +61,53 @@ function Home() {
             </div>
           </div>
          </div>
+      </section>
+      {/* Category Preview */}
+      <section>
+        <div className="container flex flex-col">
+          <h2 className="category-section-title">Shop by Category</h2>
+          <div className="w-full flex flex-wrap items-start justify-between">
+            <Link to="/products?category=computers" className="category-card">
+             <img src="https://images.unsplash.com/photo-1717390996865-57d607483101?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800" 
+             alt="Computers"
+             className="category-img" />
+             <div className="category-card-overlay" />
+             <div className="category-card-content">
+              <div className="category-icon-box">
+                <Laptop className="category-icon"/>
+              </div>
+              <h3 className="category-card-title">Computers</h3>
+              <p className="category-card-text">12+ products</p>
+             </div>
+            </Link>
+            <Link to="/products?category=smartphones" className="category-card">
+             <img src="https://images.unsplash.com/photo-1697545806152-dcbf88b3befb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800" 
+             alt="Smartphones"
+             className="category-img" />
+             <div className="category-card-overlay" />
+             <div className="category-card-content">
+              <div className="category-icon-box">
+                <Smartphone className="category-icon"/>
+              </div>
+              <h3 className="category-card-title">Smartphones</h3>
+              <p className="category-card-text">11+ products</p>
+             </div>
+            </Link>
+            <Link to="/products?category=accessories" className="category-card">
+             <img src="https://images.unsplash.com/photo-1693279504914-d08266ecbe66?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800" 
+             alt="Accessories"
+             className="category-img" />
+             <div className="category-card-overlay" />
+             <div className="category-card-content">
+              <div className="category-icon-box">
+                <Headphones className="category-icon"/>
+              </div>
+              <h3 className="category-card-title">Accessories</h3>
+              <p className="category-card-text">12+ products</p>
+             </div>
+            </Link>
+          </div>
+        </div>
       </section>
     </div>
   )
