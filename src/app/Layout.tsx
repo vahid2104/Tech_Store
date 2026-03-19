bdf92c5b5c7ee7348d5f2a63a1ff9dc4763005e3
vahid2104 <vahid.aliyev.03@mail.ru>
2026-03-19 11:43:40 +0100

    add scroll behaviour when change routes

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function Layout() {
   return (
     <div className="flex flex-col">
+      <ScrollToTop />
       <Navbar />
       <main>
         <Outlet />
